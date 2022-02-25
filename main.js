b53689c1db75513cb8f0a1eae0d7ff65b1219081
uci_dev <uciel.lud@gmail.com>
2022-02-25 00:02:43 -0300

    Se llega a un codigo mas legible

--- main.js
+++ main.js
@@ -4,6 +4,27 @@ const botonGuardar = document.querySelector('#guardar');
 
 botonGuardar.onclick = function() {
 
+    cashBoxClosing()
+}
+
+
+function cashBoxClosing(){
+    
+   
+   let data = getAllData()
+
+    multiplyingAmountsOfMoney(data.moneyDeposited)
+
+    totalSalePerBox(data.moneyDeposited)
+
+    showDataOfThePersonInCharge(data.userData)
+}
+
+function getAllData () {
+
+    const $userName = document.querySelector('#user-name').value;
+    const $turn = document.querySelector('#turn').value;
+
     const $mil = document.querySelector('#mil').value;
     const $quinientos = document.querySelector('#quinientos').value;
     const $doscientos = document.querySelector('#doscientos').value;
@@ -14,93 +35,90 @@ botonGuardar.onclick = function() {
     const $monedas = document.querySelector('#monedas').value;
     const $tarjetas = document.querySelector('#tarjetas').value;
     const $pagoProveedores = document.querySelector('#pago-proveedores').value;
-    const $ticketCliente = document.querySelector('#ticket-cliente').value;
+    const $deudaCliente = document.querySelector('#deuda-cliente').value;
     const $ticketSueldo = document.querySelector('#ticket-sueldo').value;
-    const $fondoCaja = document.querySelector('#fondo-caja').value;
-
-    console.log(
-        $mil, 
-        $quinientos, 
-        $doscientos, 
-        $cien, 
-        $cincuenta, 
-        $veinte, 
-        $diez, 
-        $monedas, 
-        $tarjetas, 
-        $pagoProveedores, 
-        $ticketCliente, 
-        $ticketSueldo, 
-        $fondoCaja
-
-    )
-
-    const totalMil = $mil * 1000;
-    const totalQuinientos = $quinientos * 500;
-    const totalDoscientos = $doscientos * 200;
-    const totalCien = $cien * 100;
-    const totalCincuenta = $cincuenta * 50;
-    const totalVeinte = $veinte * 20;
-    const totalDiez = $diez * 10;
-
-    console.log(
-
-        totalMil,
-        totalQuinientos,
-        totalDoscientos,
-        totalCien,
-        totalCincuenta,
-        totalVeinte,
-        totalDiez
-    )
-
-    let sumaActivos = [
-        totalMil, 
-        totalQuinientos, 
-        totalDoscientos, 
-        totalCien, 
-        totalCincuenta, 
-        totalVeinte, 
-        totalDiez, 
-        $monedas, 
-        $tarjetas, 
-        $pagoProveedores,
-        $ticketCliente,
-        $ticketSueldo 
-    ]
 
 
-    let sumaTotalDeActivos = 0;
-
-    console.log(sumaActivos.length)
-
-    for(let i = 0; i < sumaActivos.length; i++){
-
-        sumaTotalDeActivos += Number(sumaActivos[i]) 
-
+    const userData = {
+        
+        'userName' : $userName,
+        'turn' : $turn,
+        'date' : formattingDate()
     }
 
-    console.log(sumaTotalDeActivos)
+
+    let moneyDeposited = {
+
+        'mil':$mil,
+        'quinientos':$quinientos,
+        'doscientos':$doscientos,
+        'cien':$cien,
+        'cincuenta':$cincuenta,
+        'veinte':$veinte,
+        'diez':$diez,
+        'monedas':$monedas,
+        'tarjetas':$tarjetas,
+        'pagoProveedores':$pagoProveedores,
+        'deudaCliente':$deudaCliente,
+        'ticketSueldo':$ticketSueldo
+    }
+
+    const cashClosingDetails = {
+        
+        'userData': userData,
+        'moneyDeposited': moneyDeposited
+    }
+
+    return cashClosingDetails
+    
 }
 
+function formattingDate(){
+    const d = new Date()
+
+    const day = d.getDate()
+    const month = d.getMonth() + 1
+    const year = d.getFullYear()
+
+    const formattedDate = `${day}/${month}/${year}`
+
+    return formattedDate
+
+}
+
+function multiplyingAmountsOfMoney( amountOfMoney ){
 
 
+    amountOfMoney.mil *= 1000;
+    amountOfMoney.quinientos *= 500;
+    amountOfMoney.doscientos *= 200;
+    amountOfMoney.cien *= 100;
+    amountOfMoney.cincuenta *= 50;
+    amountOfMoney.veinte *= 20;
+    amountOfMoney.diez *= 10;
+
+}
+
+function totalSalePerBox( amountOfMoney ){
+    
+    let totalSale = 0;
+
+    for(const money in amountOfMoney){
+    
+        totalSale += Number(amountOfMoney[money]) 
+    
+    }
 
 
+}
 
+function showDataOfThePersonInCharge(userData){
 
+    const $responsableText = document.createTextNode(`Responsable: ${userData.userName}`)
+    const $turnText = document.createTextNode(`Turno: ${userData.turn}`)
+    const $dateText = document.createTextNode(`Fecha: ${userData.date}`)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    document.querySelector('#show-responsible').appendChild($responsableText)
+    document.querySelector('#show-turn').appendChild($turnText)
+    document.querySelector('#show-date').appendChild($dateText)
+}
